a868cc3f9a8ad4248e2115d91c9d465493d2c053
yukarinoki <yukarinoki@naopc.local>
2020-11-16 17:12:08 +0900

    fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
-import TOC from "../components/TOC"
+import TOC from "../components/toc"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
